1d3be58a28651fb5101aede6b0842d82c24130f7
Nigel Thornton Clark <nigeltc@gmail.com>
2021-07-04 11:59:25 -0400

    Add game engine

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import tcod
 
 from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
 from input_handlers import EventHandler
 
@@ -22,7 +23,10 @@ def main() -> None:
                  int(screen_height/2),
                  "@",
                  (255, 255, 0))
-    entities = [npc, player]
+    entities = {npc, player}   # set?
+    engine = Engine(entities=entities,
+                    event_handler=event_handler,
+                    player=player)
     
     with tcod.context.new_terminal(
             screen_width,
@@ -32,22 +36,9 @@ def main() -> None:
             vsync=True) as context:
         root_console= tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y,
-                               string=player.char,
-                               fg=player.color)
-            context.present(root_console)
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player.move(dx=action.dx,
-                                dy=action.dy)
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
             
 
