1ee4bd7b0afb9d75d91dec7c12884ab2bf8051cb
Nigel Thornton Clark <nigeltc@gmail.com>
2021-07-05 15:27:12 -0400

    Move work to action classes

--- actions.py
+++ actions.py
@@ -1,14 +1,28 @@
 """
 Game actions
 """
+#from engine import Engine
+#from entity import Entity
 
 class Action:
     """Base action"""
-    pass
+
+    #def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self, engine, entity):
+        """
+        Perform this action. 
+        ENGINE is the scope for the action
+        ENTITY is the object performing the action
+        """
+        raise NotImplementedError()
+
 
 class EscapeAction(Action):
     """ESC Action"""
-    pass
+
+    def perform(self, engine, entity):
+        raise SystemExit()
+    
 
 class MovementAction(Action):
     """Player movement"""
@@ -17,3 +31,15 @@ class MovementAction(Action):
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine, entity):
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            # destination is out of bounds
+            return
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            # destination is blocked
+            return
+        entity.move(self.dx, self.dy)
