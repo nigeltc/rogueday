630c10b9ac20aedf04a83f57cab9d9e96c45cf18
Nigel Thornton Clark <nigeltc@gmail.com>
2021-07-31 16:56:11 -0400

    Add death

--- components/fighter.py
+++ components/fighter.py
@@ -4,6 +4,8 @@ Fighter component
 from .base_component import BaseComponent
 
 class Fighter(BaseComponent):
+    entity = None
+    
     def __init__(self, hp, defense, power):
         self.max_hp = hp
         self._hp = hp
@@ -17,4 +19,19 @@ class Fighter(BaseComponent):
     @hp.setter
     def hp(self, value):
         self._hp = max(0, min(value, self.max_hp))
+        if self.hp == 0 and self.entity.ai:
+            self.die()
 
+    def die(self):
+        if self.engine.player is self.entity:
+            death_message = "You died!"
+        else:
+            death_message = f"{self.entity.name} is dead."
+        self.entity.char = "%"
+        self.entity.color = (191, 0, 0)
+        self.entity.blocks_movement = False
+        self.entity.ai = None
+        self.entity.name = f"remains of {self.entity.name}"
+        print(death_message)
+
+    
